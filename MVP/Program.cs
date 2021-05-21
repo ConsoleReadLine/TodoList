f4c4d76fc48febd6cf5872193601d0dc0c0aba19
ConsoleReadLine <81621149+ConsoleReadLine@users.noreply.github.com>
2021-05-21 21:25:42 +0300

    Add files via upload

---
+++ MVP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MVP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                ToDoList todo = new ToDoList();
+                todo.PrintText();
+                todo.Name = Console.ReadLine();
+                Console.WriteLine();
+                todo.PrintTask();
+                todo.Task = Console.ReadLine();
+                Console.WriteLine();
+                todo.PrintDate();
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
