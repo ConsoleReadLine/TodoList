1f03dbbbfb1cad3a499644aa36d9a6536eaab25a
ConsoleReadLine <jdoe249195@gmail.com>
2021-05-21 22:50:08 +0300

    Rename solution and project

---
+++ ToDoList.ConsoleApp/ToDoList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace ToDoList.ConsoleApp
+{
+    class ToDoList
+    {
+        public ToDoList()
+        {
+            
+        }
+        private string name;
+        private string task;
+        public DateTime createDate { get; set; } = DateTime.Now;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Task
+        {
+            get { return task; }
+            set { task = value; }
+        }
+
+        public void PrintText()
+        {
+            Console.WriteLine($"Введите название задачи: {Name}");
+        }
+        public void PrintTask()
+        {
+            Console.WriteLine($"Введите задачу: {Task}");
+        }
+        public void PrintDate()
+        {
+            Console.WriteLine($"Время создания задачи: {createDate}");
+        }
+    }
+}
